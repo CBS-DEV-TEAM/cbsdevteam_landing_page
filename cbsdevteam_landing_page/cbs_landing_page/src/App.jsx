b2bd88a274cecc7274569223067700367b20c1af
Cbs-Cpu <danicobosacosta2@gmail.com>
2025-07-20 11:00:34 +0200

    Actualizacion de mario

---
+++ cbsdevteam_landing_page/cbs_landing_page/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './index.css'
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Bento from './components/Bento.jsx'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <main className="h-screen flex mt-35 mx-40 items-center flex-col">
+        <Hero />
+        <Bento />
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ cbsdevteam_landing_page/cbs_landing_page/src/components/Bento.jsx
@@ -0,0 +1,19 @@
+import './Bento.css'
+
+function Bento(){
+    return(
+        <>
+            <div className="w-250 min-h-200 m-50 z-10 bentoBox">
+                <div className="bg-blue-300/30 backdrop-blur-sm rounded-2xl"></div>
+                <div className="bg-blue-300/30 backdrop-blur-sm rounded-2xl"></div>
+                <div className="bg-blue-300/30 backdrop-blur-sm rounded-2xl"></div>
+                <div className="bg-blue-300/30 backdrop-blur-sm rounded-2xl"></div>
+            </div>
+            <div className='mt-50'>
+                &nbsp; {/*Pochez para dejar espacio vacio en la pagina pal scroll*/}
+            </div>
+        </>
+    )
+}
+
+export default Bento;
